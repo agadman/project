d0fd00edf07d6e31cfc761cffc9716ba58427a87
Annika Gadman <annika.gadman@gmail.com>
2025-10-20 12:38:16 -0500

    skapat TodoItem modell och testat i Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       // Testar att skapa ett nytt TodoItem-objekt
+        TodoItem todo = new TodoItem();
+        todo.Title = "Handla";
+        todo.Description = "Storhandla mat och dryck";
+        todo.IsCompleted = false;
+        todo.DueDate = DateTime.Now.AddDays(2); //lägger in 2 dagar från idag
+
+        // Skriver ut egenskaperna för test
+        Console.WriteLine("Titel: " + todo.Title);
+        Console.WriteLine("Beskrivning: " + todo.Description);
+        Console.WriteLine("Klar? " + (todo.IsCompleted ? "Ja" : "Nej"));
+        Console.WriteLine("Sista datum: " + todo.DueDate?.ToString("yyyy-MM-dd"));  
+    }
+}
--- obj/Debug/net9.0/project.AssemblyInfo.cs
+++ obj/Debug/net9.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb93556bc2dd0fba77ae16b0085f6965f89ecd36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa64d9e89858c6fb2397d4c3a7f73bbc8cd81a66")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
