aa64d9e89858c6fb2397d4c3a7f73bbc8cd81a66
Annika Gadman <annika.gadman@gmail.com>
2025-10-20 12:23:06 -0500

    run project

--- obj/Debug/net9.0/project.AssemblyInfo.cs
+++ obj/Debug/net9.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb93556bc2dd0fba77ae16b0085f6965f89ecd36")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
